6f24c4ebf68c431cb9221745996855d6e1f24e43
spidyxx <torsten.veltum@googlemail.com>
2017-07-27 13:53:06 +0200

    all buttons placed and initialized.

--- Taschenrechner/MainWindow.xaml.cs
+++ Taschenrechner/MainWindow.xaml.cs
@@ -20,9 +20,90 @@ namespace Taschenrechner
     /// </summary>
     public partial class MainWindow : Window
     {
+        int aktuellerWert = 0;
+        bool numberinputrunning = true;
+        char lastoperation = '=';
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void appendnumber(int z)
+        {
+            numberinputrunning = true;
+            aktuellerWert = aktuellerWert * 10 + z;
+            textBlockAusgabe.Text = aktuellerWert.ToString();
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(7);
+        }       
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(8);
+        }
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(9);
+        }
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(4);
+        }
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(5);
+        }
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(6);
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(1);
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(2);
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(3);
+        }
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            appendnumber(0);
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            numberinputrunning = false;
+            lastoperation = '+';
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            numberinputrunning = false;
+            lastoperation = '-';
+        }
+
+        private void ButtonMal_Click(object sender, RoutedEventArgs e)
+        {
+            numberinputrunning = false;
+            lastoperation = '*';
+        }
+
+        private void ButtonGeteilt_Click(object sender, RoutedEventArgs e)
+        {
+            numberinputrunning = false;
+            lastoperation = '/';
+        }
+
+        private void ButtonGleich_Click(object sender, RoutedEventArgs e)
+        {
+            numberinputrunning = false;
+            lastoperation = '=';
         }
     }
 }
